d6301066cfc07d7f963cd3be7c9d398ef9008313
etiago <tiago@espinhas.net>
2013-03-10 11:54:58 +0100

    Fix checkstyle warnings

--- web/js/init.js
+++ web/js/init.js
@@ -3,7 +3,7 @@ $(document).bind("mobileinit", function(){
 	$( 'div[data-role="page"]' ).live( 'pageshow',function(event){
 		window.pageID = event.target.id;
 		
-		if (!(typeof window.Buzz.iosocket === 'undefined') && window.Buzz.iosocket !== null) {
+		if (typeof window.Buzz.iosocket !== 'undefined' && window.Buzz.iosocket !== null) {
 			if (window.pageID == "account") {
 				//window.Buzz.functions.pagebeforeshow["account"]();
 			} else {
@@ -25,9 +25,9 @@ $(document).bind("mobileinit", function(){
 		
 		// Tiago: Still not fully convinced by this solution. Should do
 		// sth more elegant rather than iterate through all.
-		if (window.Buzz.handlers != null) {
+		if (window.Buzz.handlers !== null) {
 			$.each(window.Buzz.handlers, function(k,v) {
-				if ($("#"+k).length == 0) return true;
+				if ($("#"+k).length === 0) return true;
 				
 				$("#"+k).unbind();
 				$("#"+k).click(v);
