a0b16a9cd07674082422cf7ff8eb797220d5227c
etiago <tiago@espinhas.net>
2013-08-14 10:49:37 +0200

    Adding some comments

--- server/ioserver.js
+++ server/ioserver.js
@@ -28,6 +28,8 @@ function handler (req, res) {
   });
 }
 
+// Up to here, all the code is meant to get node.js to spawn an HTTP server
+
 function RegistrationClosure(socket, data, user_by_hash) {
 	this.go = (function() {
 		return function(err, user) {
@@ -75,38 +77,51 @@ function RegistrationClosure(socket, data, user_by_hash) {
 	})();
 }
 
+// React to connection on socket.io
 io.sockets.on('connection', function(socket) {
+	// Client needs a status update, delegate to ioEventStatus
 	socket.on('status', (ioEventStatus(socket, false)));
 	
+	// Client is submitting an answer
 	socket.on('answer', (function(socket){
 		return function(data) {
 			console.log("Got an answer with data %j",data);
 			
+			// First try to find the user in the database
 			db.users.findOne({username:data.username,hash:data.hash}, (function(socket,data) {
 				return function(err, user) {
+					// User exists
 					if (user != null) {
 						console.log("User is not null and is %j", user);
 						
+						// Next we check what's the current "step" - the user is answering
+						// a question. Is the game at a stage for answering questions?
 						var stepsCursor = db.steps.find().limit(1).sort({step:"1"}, (function(socket,data,user){
 							return function(err, step) {
 								step = step[0];
 								
 								console.log("Found the step %j",step);
 								
+								// Alas, we're not answering questions... just return. Something fishy was going on
+								// on the client side. Trying to hack?
 								if (step.screen != "question") {
 									// We're not answering questions... skip?
 									
 									return;
 								}
 								
+								// If we got here, the game is accepting answers, get the respective question
 								db.questions.findOne({_id:step.fkey}, (function(socket,data,user) {
 									return function(err, question) {
 										console.log("Found the question %j",question);
 										
+										// Initially the database does not have the participants property,
+										// add it if necessary
 										if (!question.hasOwnProperty("participants")) {
 											question.participants = [];
 										}
 										
+										// If the user already responded, return, do nothing
 										if (question.participants.indexOf(user.username)!=-1) {
 											// Already responded, do nothing.
 											console.log("Already responded");
@@ -114,22 +129,24 @@ io.sockets.on('connection', function(socket) {
 											return;
 										}
 										
+										// Answer is out of bounds of possible answers, return, do nothing
 										if (data.answer >= question.answers.length || data.answer < 0) {
 											// Out of bounds, ignore.
 											console.log("Out of bounds");
 											return;
 										}
 										
-										
-										
+										// Question might not have results yet, so create it if needed
 										if (!question.hasOwnProperty("results")) {
 											question.results = new Object();
 										}
 										
+										// The counter per answer might have also not been initialized
 										if (!question.results.hasOwnProperty(data.answer)) {
 											question.results[data.answer] = [];
 										}
 										
+										// The result count also needs initializing
 										if (!question.hasOwnProperty("result_count")) {
 											question.result_count = new Object();
 										}
@@ -138,6 +155,8 @@ io.sockets.on('connection', function(socket) {
 											question.result_count[data.answer] = 0;
 										}
 										
+										// Now we're ready to push the username into the participants' list,
+										// into the answer lists and into the counter
 										question.participants.push(user.username);
 										
 										question.results[data.answer].push(user.username);
@@ -146,6 +165,7 @@ io.sockets.on('connection', function(socket) {
 										
 										db.questions.save(question);
 										
+										// Additional check: if the answer is correct, add it to the user
 										if (question.rightanswer == data.answer) {
 											user.correctAnswers++;
 											
@@ -167,17 +187,24 @@ io.sockets.on('connection', function(socket) {
 		};
 	})(socket));
 	
+	
+	// Client wants to register
 	socket.on('registration', (function(socket) { 
 		return function(data) {
+			// The registration callback has two possible actions
 			if (data.action == "register") {
+				// If loginhash is diff from null, user is trying to change names
 				if (data.loginHash != null) {
+					// We have a loginhash, get it from the db to find a possibly existing user
 					db.users.findOne({hash:data.loginHash},(function(socket,data) {
 						return function(err, user) {
+							// User exists, change his name
 							if (user != null && data.username != "") {
 								db.users.findOne({username:data.username}, new RegistrationClosure(socket,data,user).go);
 
 								return;
 							} else {
+								// Trying to change the name of a non-existing user? Emit error
 								var retObj = new Object();
 								retObj.status = "error";
 								retObj.error = "nonexistant_hash";
@@ -191,6 +218,7 @@ io.sockets.on('connection', function(socket) {
 					return;
 				}
 				
+				// Login hash was null so user is trying to register for the 1st time
 				if (data.username != null) {
 					db.users.findOne({username:data.username},new RegistrationClosure(socket,data).go);
 					
@@ -198,6 +226,7 @@ io.sockets.on('connection', function(socket) {
 				}				
 			}
 
+			// Client is requesting a checkhash, to retrieve username and language
 			if (data.action == "checkHash") {
 				var retObj = new Object();
 				if (data.loginHash === "") {
@@ -231,8 +260,9 @@ io.sockets.on('connection', function(socket) {
 		};
 	})(socket));
 
+	// This sets a event which is triggered by the PSK
+	// in order for the admin panel to be able to send commands
 	db.configs.findOne({_id:"private_key"}, (setCommandHook(socket)));
-	//redisClient.get("private_key", (setCommandHook(socket)));
 });
 
 function setCommandHook(sck) {
